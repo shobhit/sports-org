9caaa1def6b4e5d2c24c7f893708631088f0299d
bwanaballen <bwanaballen@a1bc4084-eb4d-0410-ba38-1b9a235521d5>
2008-05-23 21:28:32 +0000

    initial svn commit

---
+++ trunk/sporg/org/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('sporg.org.views',
+     (r'^$', 'index'),
+     (r'^(?P<league_id>\d+)/league/$', 'league'),
+     (r'^(?P<division_id>\d+)/division/$', 'division'),
+     (r'^(?P<league_id>\d+)/league/(?P<team_id>\d+)/team/$', 'team'),
+     (r'^(?P<league_id>\d+)/league/(?P<team_id>\d+)/team/(?P<player_id>\d+)/player/$', 'player'),
+     (r'^/schedule/$', 'schedule0'),
+     (r'^(?P<league_id>\d+)/league/schedule/$', 'schedule1'),
+     (r'^(?P<league_id>\d+)/league/(?P<team_id>\d+)/team/schedule/$', 'schedule2'),
+     (r'^/results/$', 'results0'),
+     (r'^(?P<league_id>\d+)/league/results/$', 'results1'),
+     (r'^(?P<league_id>\d+)/league/(?P<team_id>\d+)/team/results/$', 'results2'),
+     (r'^/standings/$', 'standings0'),
+     (r'^(?P<league_id>\d+)/league/standings/$', 'standings1'),
+     (r'^(?P<league_id>\d+)/league/(?P<team_id>\d+)/team/standings/$', 'standings2'),
+     (r'^(?P<league_id>\d+)/league/build_schedule/$', 'build_schedule'),
+     (r'^(?P<league_id>\d+)/league/build_availability/$', 'build_availability'),
+     (r'^/accept_availability/$', 'accept_availability'),
+)
+
